503a9a71a926bcf0bccd3a1a4ad3d4eb51aa0c91
Gehbauer, Jan <jan.gehbauer@zuehlke.com>
2021-09-27 19:51:36 +0200

    create user profile service

---
+++ frontend/src/app/features/user-profile/services/user-profile.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@angular/core";
+import {Observable, of} from "rxjs";
+import {User} from "@base/shared/models/user";
+import {Category} from "@base/shared/models/category";
+import {Interest} from "@base/shared/models/interest";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserProfileService {
+
+  public getCurrentlyLoggedInUser(): Observable<User> {
+    return of(new User("Test user :D", "", [
+      new Category(1, "sports",[new Interest(1, "soccer"), new Interest(2, "tennis")]),
+      new Category(2, "music",[new Interest(3, "trumpet"), new Interest(4, "trombone")])
+    ])); // TODO: replace
+  }
+}
