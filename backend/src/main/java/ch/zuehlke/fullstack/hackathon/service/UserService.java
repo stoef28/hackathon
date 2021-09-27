22ffb33740a03743d7be8fccd420cd89fb5274cd
Gezim Zeneli <g_zeneli@hotmail.com>
2021-09-27 16:45:16 +0200

    User Service added

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/UserService.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/UserService.java
@@ -1,21 +1,51 @@
 package ch.zuehlke.fullstack.hackathon.service;
 
-import ch.zuehlke.fullstack.hackathon.model.Interest;
-import ch.zuehlke.fullstack.hackathon.model.User;
+import ch.zuehlke.fullstack.hackathon.model.*;
+import ch.zuehlke.fullstack.hackathon.service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.lang.module.FindException;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
 
-    public User getUserBy(long id){
-        return null;
+
+    public User getUserBy(long id) {
+        return userRepository.findById(id).orElseThrow( () -> new FindException());
     }
 
     public User addInterestToUser(Interest interest, User user) {
-        return null;
+        Collection<Interest> interestCollection = user.getInterestCollection();
+        interestCollection.add(interest);
+        user.setInterestCollection(interestCollection);
+
+        return userRepository.save(user);
     }
 
-    public User removeInterestFromUser(Interest interest, User user){
-        return null;
+    public User removeInterestFromUser(Interest interest, User user) {
+        Collection<Interest> interestCollection = user.getInterestCollection();
+        interestCollection = interestCollection.stream().filter(interestIter -> interestIter.getInterestID() != interest.getInterestID()).collect(Collectors.toList());
+        user.setInterestCollection(interestCollection);
+
+        return userRepository.save(user);
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(User user) {
+        userRepository.delete(user);
     }
 
 }
---
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ch.zuehlke.fullstack.hackathon.service.repository;
+
+import ch.zuehlke.fullstack.hackathon.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
