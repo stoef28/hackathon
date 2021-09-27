c21ab4266d727848622186d8197b8a4cc9572a2f
steb <stefan.boesch@zuehlke.com>
2021-09-27 15:17:38 +0200

    Added empty UserService

---
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/UserService.java
@@ -0,0 +1,21 @@
+package ch.zuehlke.fullstack.hackathon.service;
+
+import ch.zuehlke.fullstack.hackathon.model.Interest;
+import ch.zuehlke.fullstack.hackathon.model.User;
+
+public class UserService {
+
+
+    public User getUserBy(long id){
+        return null;
+    }
+
+    public User addInterestToUser(Interest interest, User user) {
+        return null;
+    }
+
+    public User removeInterestFromUser(Interest interest, User user){
+        return null;
+    }
+
+}
