9a6fba65e41f8ca2e09a83db9908d97888ea3eed
Gehbauer, Jan <jan.gehbauer@zuehlke.com>
2021-09-27 17:17:35 +0200

    remove deleted component from test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import {TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
-import {ExampleComponent} from "./example-component/example.component";
 import {HttpClientModule} from "@angular/common/http";
 
 describe('AppComponent', () => {
@@ -13,7 +12,6 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
-        ExampleComponent
       ],
     }).compileComponents();
   });
