1efa23bfa3db9eb190bbc7fcce75652478785de4
Gehbauer, Jan <jan.gehbauer@zuehlke.com>
2021-09-27 19:50:35 +0200

    update user profile component

--- frontend/src/app/features/user-profile/container/user-profile.component.ts
+++ frontend/src/app/features/user-profile/container/user-profile.component.ts
@@ -1,9 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {UserProfileService} from "@base/features/user-profile/services/user-profile.service";
+import {User} from "@base/shared/models/user";
 
 @Component({
 	selector: "user-profile",
 	templateUrl: "./user-profile.component.html",
 	styleUrls: ["./user-profile.component.scss"]
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+
+  user!: User;
+
+  constructor(private userProfileService: UserProfileService) {
+  }
+
+  ngOnInit(): void {
+    this.userProfileService.getCurrentlyLoggedInUser().subscribe(user => this.user = user);
+  }
+
 }
--- frontend/src/app/features/user-profile/user-profile.module.ts
+++ frontend/src/app/features/user-profile/user-profile.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from "@angular/core";
 import {UserProfileComponent} from "@base/features/user-profile/container/user-profile.component";
 import {UserProfileRoutingModule} from "@base/features/user-profile/user-profile-routing.module";
+import {UserCategoriesComponent} from "@base/features/user-profile/components/user-categories/user-categories.component";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
-    UserProfileComponent
+    UserProfileComponent,
+    UserCategoriesComponent
   ],
-  imports: [UserProfileRoutingModule]
+  imports: [UserProfileRoutingModule, CommonModule]
 })
 export class UserProfileModule {}
