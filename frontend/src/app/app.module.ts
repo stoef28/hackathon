cca6585ee8a5f970422e6f5f217b7bf57517f782
Gehbauer, Jan <jan.gehbauer@zuehlke.com>
2021-09-27 17:06:57 +0200

    update app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import {NavbarModule} from "@base/features/navbar/navbar.module";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
