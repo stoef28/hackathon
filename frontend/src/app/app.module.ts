13f14876f655999a994a2ff3833250314ea2392a
Gehbauer, Jan <jan.gehbauer@zuehlke.com>
2021-09-27 15:05:17 +0200

    remove example component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {ExampleComponent} from './example-component/example.component';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ExampleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
