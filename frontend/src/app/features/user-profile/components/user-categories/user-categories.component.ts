756b937a8ac2f890d60a9fdd10bd2e7338213559
Gehbauer, Jan <jan.gehbauer@zuehlke.com>
2021-09-28 13:26:07 +0200

    adjust component selectors

--- frontend/src/app/features/user-profile/components/user-categories/user-categories.component.ts
+++ frontend/src/app/features/user-profile/components/user-categories/user-categories.component.ts
@@ -2,7 +2,7 @@ import {Component, Input} from "@angular/core";
 import {Category} from "@base/shared/models/category";
 
 @Component({
-	selector: "user-categories",
+	selector: "app-user-categories",
 	templateUrl: "./user-categories.component.html",
 	styleUrls: ["./user-categories.component.scss"]
 })
